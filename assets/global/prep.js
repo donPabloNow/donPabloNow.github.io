c894602f19e32e96a06b75f98ee42859c4a4eedd
don Pablo <don@obeyi.com>
2020-10-28 22:58:39 +0200

    update

--- assets/global/prep.js
+++ assets/global/prep.js
@@ -1,6 +1,7 @@
 if(document.URL.toLowerCase().includes('#meet')) setTimeout(() => {goMeeting()}, 100);
 if(document.URL.toLowerCase().includes('#hire')) setTimeout(() => {goHire()}, 100);
 if(document.URL.toLowerCase().includes('#resume')) setTimeout(() => {goResume()}, 100);
+if(document.URL.toLowerCase().includes('#live')) setTimeout(() => {goLive()}, 100);
 
         function ReplaceContent(NC) {
         document.body.remove();
@@ -11,6 +12,10 @@ if(document.URL.toLowerCase().includes('#resume')) setTimeout(() => {goResume()}
 
       function goMeeting() {
         ReplaceContent(`<!DOCTYPE html><html lang="en"><head> <meta charset="utf-8" /> <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png"> <link rel="icon" type="image/x-icon" href="./favicon.ico"> <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" /> <title> Meeting with (don) Pablo Brincat </title> <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' /></head><body id="navIntro" class="web-page presentation-page sidebar-collapse"> <center> <a href="https://donpablonow.com/"> <img alt="don Pablo" src="https://donpablonow.com/assets/img/signature.png" height="100px" /> </a> </center> <!-- Start of Meetings Embed Script --> <div class="meetings-iframe-container" data-src="https://meetings.hubspot.com/pablo-brin?embed=true"></div> <script type="text/javascript" src="https://static.hsappstatic.net/MeetingsEmbed/ex/MeetingsEmbedCode.js"></script> <!-- End of Meetings Embed Script --> <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/7223486.js"></script> <script async defer type="text/javascript" id="slcLiveChat" src="https://widget.sonetel.com/SonetelWidget.min.js" data-account-id="207161307"> </script><!-- Global site tag (gtag.js) - Google Analytics --><script async src="https://www.googletagmanager.com/gtag/js?id=UA-161901594-1"></script><script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-161901594-1');</script></body></html>`);
+      }
+
+      function goLive() {
+        ReplaceContent(`<html lang=en><head><meta charset=utf-8 /><link rel=apple-touch-icon sizes=76x76 href=./assets/img/apple-icon.png><link rel=icon type=image/x-icon href=./favicon.ico><meta http-equiv=X-UA-Compatible content=IE=edge,chrome=1 /><title>Meeting with (don) Pablo Brincat</title><meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name=viewport /></head><body id=navIntro class="web-page presentation-page sidebar-collapse"><center><a href=https://donpablonow.com/><img alt="don Pablo" src=https://donpablonow.com/assets/img/signature.png height=100px /></a></center> <figure><embed src="https://wakatime.com/share/@donPabloNow/6131e769-af1f-44e2-bffd-5a6723b238cd.svg"></embed></figure> <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/7223486.js"></script> <script async defer type="text/javascript" id="slcLiveChat" src="https://widget.sonetel.com/SonetelWidget.min.js" data-account-id="207161307"> </script> <script async src="https://www.googletagmanager.com/gtag/js?id=UA-161901594-1"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-161901594-1');</script></body></html>`);
       }
 
 
